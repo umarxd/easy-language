62016fc1377a49c1d528f839bdb9eff1be14426d
umarxd <xqcowsavedmylife@gmail.com>
2023-10-27 22:25:03 +0300

    add subjects section for main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Icons } from "@/components/Icons";
+import Subjects from "@/components/Subjects";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="my-12">
+    <div className="my-12 px-2">
       <div className="flex flex-col gap-2 justify-center items-center text-center ">
         <Image
           className="mx-auto"
@@ -18,9 +20,20 @@ export default function Home() {
           a translator if you get stuck as this website is completely in
           English.
         </p>
-        <p className="text-sm mt-4">
-          If you would like to contact me here are my socials.
-        </p>
+
+        <Subjects />
+
+        <div className="text-sm mt-6">
+          <p>If you would like to contact me here are my socials.</p>
+          <div className="flex gap-2 justify-center mt-1">
+            <a target="_blank" href="https://github.com/umarxd">
+              <Icons.githubLogo width={24} height={24} />
+            </a>
+            <a target="_blank" href="https://twitter.com/eomer___">
+              <Icons.twitterLogo width={24} height={24} />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Subjects.tsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+
+export default function Subjects() {
+  return (
+    <div className="mt-4 flex flex-wrap gap-3 w-2/3 flex-shrink justify-center">
+      <div className="">
+        <Link className="text-blue-500 hover:text-blue-100" href={"/verbs"}>
+          Verbs and Verb Tenses
+        </Link>
+        <p>
+          Action verbs, express what is happening (do, work). State verbs
+          express a situation (be, have).
+        </p>
+      </div>
+      <div className="">
+        <Link
+          className="text-blue-500 hover:text-blue-100"
+          href={"/adjectives"}
+        >
+          Adjectives
+        </Link>
+        <p>
+          An adjective is a word that tells us more about a noun (big, red,
+          expensive).
+        </p>
+      </div>
+      <div className="">
+        <Link className="text-blue-500 hover:text-blue-100" href={"/adverbs"}>
+          Adverbs
+        </Link>
+        <p>
+          Adverbs tell us more about verbs, adjectives or adverbs (loudly,
+          really, extremely).
+        </p>
+      </div>
+      <div className="">
+        <Link
+          className="text-blue-500 hover:text-blue-100"
+          href={"/determiners"}
+        >
+          Determiners
+        </Link>
+        <p>
+          Determiners are words like the, an, this that start a noun phrase.
+        </p>
+      </div>
+      <div className="">
+        <Link className="text-blue-500 hover:text-blue-100" href={"/pronouns"}>
+          Pronouns
+        </Link>
+        <p>
+          Pronouns are small words like you, ours, some that can take the place
+          of a noun.
+        </p>
+      </div>
+      <div className="">
+        <Link
+          className="text-blue-500 hover:text-blue-100"
+          href={"/conjunctions"}
+        >
+          Conjunctions
+        </Link>
+        <p>Conjunctions join two parts of a sentence (and, but, though).</p>
+      </div>
+      <div className="">
+        <Link
+          className="text-blue-500 hover:text-blue-100"
+          href={"/interjections"}
+        >
+          Interjections
+        </Link>
+        <p>Short exclamations with no real grammatical value (ah, dear, er)</p>
+      </div>
+    </div>
+  );
+}
